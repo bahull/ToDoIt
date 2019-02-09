d37ae836c02cf10bcf9c6e9db04af92c0b0c79f5
Bryce Hull <bahull@live.com>
2019-02-09 13:39:00 -0600

    Initial Commit

---
+++ ToDoIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoIt
+//
+//  Created by Bryce Hull on 2/9/19.
+//  Copyright © 2019 Bryce Hull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
