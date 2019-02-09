b2ad33875b33859c2950d6f9498f11f22d43d7b9
Bryce Hull <bahull@live.com>
2019-02-09 15:08:01 -0600

    Update alert to accept new entries

--- ToDoIt/TodoListViewController.swift
+++ ToDoIt/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,12 +55,24 @@ class TodoListViewController: UITableViewController {
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
+        var textField = UITextField()
+        
         let alert = UIAlertController(title: "Add New ToDoIt Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks add item button on the UI Alert
-            print("Success!")
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
         }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
         
         alert.addAction(action)
         
