ea4b68cd21c27f5413c974de85abd8f0c91ee6d2
Bryce Hull <bahull@live.com>
2019-02-09 16:02:43 -0600

    Save user defaults in local .plist

--- ToDoIt/TodoListViewController.swift
+++ ToDoIt/TodoListViewController.swift
@@ -11,10 +11,16 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //Mark - Table Datasource Methods
@@ -63,6 +69,8 @@ class TodoListViewController: UITableViewController {
             //what will happen once the user clicks add item button on the UI Alert
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
             
