48920b07b52b814cb4078de38f8681567642646d
Bryce Hull <bahull@live.com>
2019-02-10 15:15:56 -0600

    Include Realm Swift

---
+++ Podfile
@@ -0,0 +1,11 @@
+ platform :ios, '9.0'
+
+target 'ToDoIt' do
+
+
+  # Pods for ToDoIt
+use_frameworks!
+
+pod 'RealmSwift'
+
+end
--- ToDoIt/AppDelegate.swift
+++ ToDoIt/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,10 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+  
+        do {
+            let realm = try Realm()
+        } catch {
+            print("Error initializing new realm, \(error)")
+        }
         
-        //How to find where user defaults are stored
-        //print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
         
         return true
     }
